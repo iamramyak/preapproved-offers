65917f5eaeed2555a591a4afa6f82bea4d56583b
iamramyak <iamramyak7@gmail.com>
2024-01-06 21:48:22 +0530

    preapproved offers

---
+++ src/main/java/com/example/preapproved/offers/program/PreapprovedOffersProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.preapproved.offers.program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreapprovedOffersProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreapprovedOffersProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/preapproved/offers/program/PreapprovedOffersProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preapproved.offers.program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreapprovedOffersProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
